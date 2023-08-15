335deea8d7c36dcc24f964c298739d2533c81eff
Fareez RW <iqmal3@outlook.com>
2023-08-15 21:31:57 +0800

    :sparkles: Completed showing chips module

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -3,9 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'message',
+    ];
+    
+    /**
+     * Get the user that owns the Chirp
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
